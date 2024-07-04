daabfc2bdae94a1a13d5877bfc927298d1f6f0c4
marasatilu <marasatilu@gmail.com>
2024-07-04 14:56:00 +0200

    update camera position

--- src/script.js
+++ src/script.js
@@ -168,7 +168,7 @@ window.addEventListener('resize', () => {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.x = 1;
 camera.position.y = 1;
-camera.position.z = 4;
+camera.position.z = 5;
 scene.add(camera);
 
 // Controls
